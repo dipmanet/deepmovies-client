8c2ad46a32af1000adfe4c31758e65f5b0fba7b1
DipMan <dipmanet2021@gmail.com>
2024-12-09 07:47:16 +0545

    updated responsiveness

---
+++ src/components/Pagination/PosterPagination.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+
+const PosterPagination = () => {
+	return <div>PosterPagination</div>;
+};
+
+export default PosterPagination;
